f0b64e9e49e7e72f021cbbbb9e8b04f7972e9dad
Siddarth Kotthur <kotthursiddarth@gmail.com>
2020-05-27 20:22:39 -0500

    first commit

---
+++ extended.js
@@ -0,0 +1,19 @@
+class conStr {
+  constructor(string, count, join) {
+    this.string = string;
+    this.count = count;
+    this.join = join;
+  }
+
+  times() {
+    let string = this.string;
+    let occ = 0;
+    while (occ != this.count - 1) {
+      string = `${string}${this.join}${this.string}`;
+      ++occ;
+    }
+    return string;
+  }
+}
+
+module.exports = conStr;
