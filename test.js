3a178d64eaf4e4df702d009c9056117e6f8e1c8a
Siddarth Kotthur <kotthursiddarth@gmail.com>
2020-09-11 21:14:05 -0500

    minor changes

--- test.js
+++
@@ -1,5 +0,0 @@
-// let { basicFunctions, replaceFunctions, extendedFunctions } = require("./strparse")
-let str = require("./strparse");
-
-let firstString = new str.replaceFunctions("strparse!strparse!", "$");
-console.log(firstString.indexRep(2, false).output);
